7059b60aebd0c477250e6322c1456cc7f045816d
milanla <milan.lanlada@gmail.com>
2018-10-01 20:34:37 -0400

    created tables

--- bin/run.rb
+++ bin/run.rb
@@ -2,4 +2,16 @@ require_relative '../config/environment'
 
 
 
-puts "HELLO WORLD"
+puts "Welcome to SuperFan the Trivia Game!"
+
+puts "===================================="
+
+puts "Input your username: "
+
+username = gets.chomp
+
+puts "What genre are you most interested in? "
+genre = gets.chomp
+# multichoice - user input a number
+
+puts "Hey #{username}! Let's see what you know about #{genre}!!!"
