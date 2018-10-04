a98ad69d10c153487ec4c8fb584bc33360c3072a
milanla <milan.lanlada@gmail.com>
2018-10-04 14:09:47 -0400

    fixed username prompt and added emojis

--- bin/methods.rb
+++ bin/methods.rb
@@ -1,24 +1,38 @@
-
 def welcome
-  puts "Welcome to SuperFan the Trivia Game!"
-  puts "===================================="
-  puts "Rules"
-  puts "===================================="
-  puts "1. Enter your username
-2. Choose an artist you feel you know the best.
-3. Answer a series of questions about that artist
-as quickly as possible while only typing “a , b or c “
-to correspond to the multiple choice answers provided."
-  puts "===================================="
+  puts "            +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+"
+  puts "            |S| |u| |p| |e| |r| |F| |a| |n|"
+  puts "            +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+"
+  puts "                 ╔════╗
+                 ║ ██ ║
+                 ║ (O)║♫ ♪ ♫ ♪
+                 ╚════╝
+               ▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █
+            Min- - - - - - - - - - - -●Max"
+
+  puts "         Welcome to SuperFan the Trivia Game!"
+  sleep(3)
+  system "clear"
+  puts "         ===================================="
+  puts "                         Rules"
+  puts "         ===================================="
+  puts "                1. Enter your username
+    2. Choose an artist you feel you know the best.
+    3. Answer a series of questions about that artist
+ as quickly as possible while only typing “a , b or c “
+ to correspond to the multiple choice answers provided."
+  puts "         ===================================="
   prompt = TTY::Prompt.new
   username = prompt.ask('Input your username:') do |q|
-    q.validate /[a-z]/
+    q.required true
+    q.validate /^\s*[a-zA-Z0-9,\s]+\s*$/
+    q.messages[:valid?] = "Invalid username"
   end
   new_player = Player.find_or_create_by(name: username)
   pick_artist(new_player)
 end
 
 def pick_artist(player)
+  system "clear"
   name = list_artists
   puts '===================================='
   artist = Artist.find_by(name: name)
@@ -49,7 +63,7 @@ end
 
 def print_questions_and_choices(artist_name, new_player,questions_array)
   questions_array.each do |q|
-    puts q.content
+    puts q.content.colorize(:color => :light_blue, :background => :red)
     puts q.choice_a
     puts q.choice_b
     puts q.choice_c
@@ -60,19 +74,22 @@ def print_questions_and_choices(artist_name, new_player,questions_array)
     if ui == q.answer
       new_player.points += 1
       puts `clear`
+      puts "            ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ"
       puts "=================================================="
-      puts "Correct! You're pretty awesome!!!"
+      puts "       Correct! You're pretty awesome!!!"
       puts "=================================================="
     elsif ui != "a" && ui != "b" && ui != "c"
       puts `clear`
+      puts "                ʕノ•ᴥ•ʔノ ︵ ┻━┻"
       puts "=================================================="
-      puts "Invalid input. Please type a, b or c."
+      puts "     Invalid input. Please type a, b or c."
       puts "=================================================="
 
     redo else
       puts `clear`
+      puts "                    ┌П┐(ಠ_ಠ)"
       puts "=================================================="
-      puts "Boo! Try harder next time!"
+      puts "               Wrong! Try harder!"
       puts "=================================================="
     end
     sleep(2)
@@ -82,9 +99,36 @@ def print_questions_and_choices(artist_name, new_player,questions_array)
 end
 
 def goodbye(new_player)
-  puts "Congratulations you have earned #{new_player.points} points!!!"
+  system "clear"
   new_player.update(points: new_player.points)
-  puts "You have a total of #{new_player.points} points!!!"
+  puts "            Congratulations! You have a total of #{new_player.points} points!!!"
+  puts "    +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-++-+ +-+ +-+ +-+ +-+ +-+ +-+ +-++-+ +-+"
+  puts "                    Thank you for playing SuperFan"
+  sleep(3)
+  system "clear"
+  puts "          +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+"
+  puts "          |S| |u| |p| |e| |r| |F| |a| |n|"
+  puts "          +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+"
+  sleep(3)
+  start_over
+end
+
+def start_over
+  prompt = TTY::Prompt.new
+  restart = prompt.select('Would like to play again?', %w(Yes No))
+# binding.pry
+  case restart
+  when "Yes"
+    welcome
+  when "No"
+    system "clear"
+    puts "          Thank you for playing SuperFan!"
+    puts "                                         "
+    puts "          +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+"
+    puts "          |S| |u| |p| |e| |r| |F| |a| |n|"
+    puts "          +-+ +-+ +-+ +-+ +-+ +-+ +-+ +-+"
+    sleep(2)
+  end
 end
 
 def list_artists
--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,66 @@
 require_relative '../config/environment'
 
 require 'pry'
-
+require 'colorize'
+require 'colorized_string'
 require_relative './methods.rb'
+
+# puts "                           *     .--."
+# puts "                                / /  `"
+# puts "               +               | |"
+# puts "                      '        \\ \\__,"
+# puts "                  *          +   '--'  *"
+# puts "                      +   /\\"
+# puts "         +              .'  '.   *"
+# puts "                *      /======\\      +"
+# puts "                      ;:.  _   ;"
+# puts "                      |:. (_)  |"
+# puts "                      |:.  _   |"
+# puts "            +         |:. (_)  |          *"
+# puts "                      ;:.      ;"
+# puts "                    .' \:.    / `."
+# puts "                   / .-'':._.'`-. \/"
+# puts "                   |/    /||\    \\|"
+# puts "                 _..--\"\"\"````\"\"\"--.._"
+# puts "           _.-'``                    ``'-._"
+# puts "         -'                                '-"
+
+# def print_questions_and_choices(artist_name, new_player,questions_array)
+#   questions_array.each do |q|
+#     qs = questions_array.map do |q|
+#             q.content
+#             binding.pry
+#           end
+#     ans = questions_array.map do |q|
+#             q.choice_a, q.choice_b, q.choice_c
+#           end
+#     prompt = TTY::Prompt.new
+#     ui = prompt.select(qs, ans)
+#     case ui
+#       when ui == q.answer
+#         new_player.points += 1
+#         puts `clear`
+#         puts "            ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟ"
+#         puts "=================================================="
+#         puts "       Correct! You're pretty awesome!!!"
+#         puts "=================================================="
+#       when ui != "a" && ui != "b" && ui != "c"
+#         puts `clear`
+#         puts "                ʕノ•ᴥ•ʔノ ︵ ┻━┻"
+#         puts "=================================================="
+#         puts "     Invalid input. Please type a, b or c."
+#         puts "=================================================="
+#
+#       redo when
+#         puts `clear`
+#         puts "                    ┌П┐(ಠ_ಠ)"
+#         puts "=================================================="
+#         puts "               Wrong! Try harder!"
+#         puts "=================================================="
+#       end
+#       sleep(2)
+#       system "clear"
+#     end
+#   end
+#   goodbye(new_player)
+# end
